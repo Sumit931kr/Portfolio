748eab5e03beacc7a8e98f733d3d672b66e0773d
Sumit <sumitkumar280601@gmail.com>
2024-12-07 00:02:15 +0530

    Add: self projects

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
 
-app.use(express.static('web'));
+app.use(express.static(path.join(__dirname, 'web')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
