a472e9a5558b1192164dc8c7b4e5f6f79a863842
Sumit <sumitkumar280601@gmail.com>
2024-12-18 23:40:43 +0530

    Add: project

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/contact', (req,res)=>{
+  console.log(req.body);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
